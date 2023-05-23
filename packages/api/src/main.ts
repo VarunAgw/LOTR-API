2f508357658dc08f11b7372aed9d4f14cf7a634a
Varun Agrawal <Varun@VarunAgw.com>
2023-05-23 16:25:11 +0200

    Configured swagger for API documentation generation

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -4,14 +4,20 @@
  */
 
 import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder().setVersion('1.0').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
